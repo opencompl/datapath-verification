60c21f19a7604b929111f90f2094c1aa2a571a7e
osmanyasar05 <osmanyas05@gmail.com>
2026-05-06 16:06:37 +0100

    compare

--- DatapathVerification/compare.lean
+++ DatapathVerification/compare.lean
@@ -1,6 +1,8 @@
 import Blase
 import DatapathVerification.CSA
 
+set_option maxHeartbeats 50000000
+set_option Elab.async false
 /-!
   This file compares the performance of bv_decide in different multiplication circuits.
 -/
@@ -10,28 +12,84 @@ namespace CSA
 
 -- Multiplication implementation based on compression of partial products.
 set_option trace.profiler true in
-theorem mul_comm_4bit (x y : BitVec 4) : mulChain x y = mulChain y x  := by
+theorem mulChain_4bit (x y : BitVec 4) : mulChain x y = mulChain y x  := by
   bv_decide
 
 set_option trace.profiler true in
-theorem mul_comm_5bit (x y : BitVec 5) : mulChain x y = mulChain y x  := by
+theorem mulChain_5bit (x y : BitVec 5) : mulChain x y = mulChain y x  := by
   bv_decide
 
 set_option trace.profiler true in
-theorem mul_comm_6bit (x y : BitVec 6) : mulChain x y = mulChain y x  := by
+theorem mulChain_6bit (x y : BitVec 6) : mulChain x y = mulChain y x  := by
   bv_decide
 
 set_option trace.profiler true in
-theorem mul_comm_7bit (x y : BitVec 7) : mulChain x y = mulChain y x  := by
+theorem mulChain_7bit (x y : BitVec 7) : mulChain x y = mulChain y x  := by
   bv_decide
 
 set_option trace.profiler true in
-theorem mul_comm_8bit (x y : BitVec 8) : mulChain x y = mulChain y x  := by
+theorem mulChain_8bit (x y : BitVec 8) : mulChain x y = mulChain y x  := by
   bv_decide
 
 set_option trace.profiler true in
-theorem mul_comm_9bit (x y : BitVec 9) : mulChain x y = mulChain y x  := by
+theorem mulChain_9bit (x y : BitVec 9) : mulChain x y = mulChain y x  := by
   bv_decide
+
+set_option trace.profiler true in
+theorem mulChain_10bit (x y : BitVec 10) : mulChain x y = mulChain y x  := by
+  bv_decide (config := { timeout := 200})
+
+set_option trace.profiler true in
+theorem mulChain_11bit (x y : BitVec 11) : mulChain x y = mulChain y x  := by
+  bv_decide (config := { timeout := 200})
+
+set_option trace.profiler true in
+theorem mulChain_opt_4bit (x y : BitVec 4) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide
+
+set_option trace.profiler true in
+theorem mulChain_opt_5bit (x y : BitVec 5) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide
+
+set_option trace.profiler true in
+theorem mulChain_opt_6bit (x y : BitVec 6) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide
+
+set_option trace.profiler true in
+theorem mulChain_opt_7bit (x y : BitVec 7) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide
+
+set_option trace.profiler true in
+theorem mulChain_opt_8bit (x y : BitVec 8) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide
+
+set_option trace.profiler true in
+theorem mulChain_opt_9bit (x y : BitVec 9) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide
+
+set_option trace.profiler true in
+theorem mulChain_opt_10bit (x y : BitVec 10) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide (config := { timeout := 200})
+
+set_option trace.profiler true in
+theorem mulChain_opt_11bit (x y : BitVec 11) : mulChain_opt x y = mulChain_opt y x  := by
+  simp [mulChain_opt, partialProducts, partialProducts']
+  repeat (unfold chain_opt; simp [List.reverse, List.reverseAux])
+  bv_decide (config := { timeout := 400})
 
 end CSA
 
@@ -72,5 +130,13 @@ theorem mul_comm_8bit (x y : BitVec 8) : mulRef x y = mulRef y x  := by
 set_option trace.profiler true in
 theorem mul_comm_9bit (x y : BitVec 9) : mulRef x y = mulRef y x  := by
     bv_decide
+
+set_option trace.profiler true in
+theorem mul_comm_10bit (x y : BitVec 10) : mulRef x y = mulRef y x  := by
+  bv_decide (config := { timeout := 200})
+
+set_option trace.profiler true in
+theorem mul_comm_11bit (x y : BitVec 11) : mulRef x y = mulRef y x  := by
+  bv_decide (config := { timeout := 200})
 
 end CSABlastMul
